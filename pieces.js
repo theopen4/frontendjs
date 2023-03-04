53c06e585c16170f18c940a6abaa94434f1a0e6b
theopen4 <kielze86@gmail.com>
2023-03-04 14:01:27 +0100

    modify elements

--- pieces.js
+++ pieces.js
@@ -1,9 +1,28 @@
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
-const article = pieces[0];
-const imageElement = document.createElement('img');
-imageElement.src = article.image;
-const pieceElement = document.createElement("article");
-const sectionFiches = document.querySelector('.fiches');
-pieceElement.appendChild(imageElement)
-sectionFiches.appendChild(pieceElement);
+ 
+for( let i = 0; i < pieces.length; i++){
+    const article = pieces[i];
+    const sectionFiches = document.querySelector('.fiches');
+    const pieceElement = document.createElement('article');
+    const imageElement = document.createElement('img');
+    imageElement.src = article.image
+    sectionFiches.appendChild(pieceElement);
+    pieceElement.appendChild(imageElement); 
+
+}
+
+/*for (let i = 0; i < pieces.length; i++) {
+
+    const article = pieces[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // Création des balises 
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+}*/
