c5b2b76eccb5f0c3e6daafe49dba8d099276f648
theopen4 <kielze86@gmail.com>
2023-03-23 19:29:37 +0100

    new code js

--- Frontend/pieces.js
+++ Frontend/pieces.js
@@ -1,68 +1,77 @@
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
-// Création des balises 
-// const article = pieces[0];
-// const imageElement = document.createElement("img");
-// imageElement.src = article.image;
-// const nomElement = document.createElement("h2");
-// nomElement.innerText = article.nom;
-// const prixElement = document.createElement("p");
-// prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-// const categorieElement = document.createElement("p");
-// categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
 
-//Création des élements pour l'exercice
-// const descriptionElement = document.createElement("p");
-// descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+for (let i = 0; i < pieces.length; i++) {
 
-// const stockElement = document.createElement("p");
-// stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
-
-// //Rattachement de nos balises au DOM
-// const sectionFiches = document.querySelector(".fiches");
-// sectionFiches.appendChild(imageElement);
-// sectionFiches.appendChild(nomElement);
-// sectionFiches.appendChild(prixElement);
-// sectionFiches.appendChild(categorieElement);
-
-// //Ajout des éléments au DOM pour l'exercice
-// sectionFiches.appendChild(descriptionElement);
-// sectionFiches.appendChild(stockElement);
-const sectionFiches = document.querySelector(".fiches");
-
-
-for(let i = 0; i < pieces.length; i++ ){
     const article = pieces[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // Création des balises 
     const imageElement = document.createElement("img");
-    imageElement.src = article.image
+    imageElement.src = article.image;
     const nomElement = document.createElement("h2");
     nomElement.innerText = article.nom;
     const prixElement = document.createElement("p");
-    prixElement.innerText = `prix: ${article.prix} $ (${article.prix < 35 ?'$' : '$$$'})`
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
     const categorieElement = document.createElement("p");
-    categorieElement.innerText = article.categorie ?? "(aucune categorie)";
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
     const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = article.description ?? "(pas de description)"
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
     const stockElement = document.createElement("p");
-    stockElement.innerText = article.disponibilite ? "en stock" : "en rupture"
-    const articleFiche = document.createElement("article");
-    articleFiche.appendChild(imageElement);
-    articleFiche.appendChild(nomElement);
-    articleFiche.appendChild(prixElement);
-    articleFiche.appendChild(categorieElement);
-    articleFiche.appendChild(descriptionElement);
-    articleFiche.appendChild(stockElement);
-    sectionFiches.appendChild(articleFiche);
-}
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    
+    // On rattache la balise article a la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    //Ajout des éléments au DOM pour l'exercice
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
+
+ }
+ 
+ //gestion des bouttons 
 const boutonTrier = document.querySelector(".btn-trier");
-boutonTrier.addEventListener("click", function(){
+
+boutonTrier.addEventListener("click", function () {
     const piecesOrdonnees = Array.from(pieces);
-    piecesOrdonnees.sort((a,b) => a.prix - b.prix);
-    console.log(piecesOrdonnees);
+    piecesOrdonnees.sort(function (a, b) {
+        return a.prix - b.prix;
+     });
+     console.log(piecesOrdonnees);
 });
-const bouttonFiltrer = document.querySelector(".btn-filtrer");
-bouttonFiltrer.addEventListener("click", function(){
-    pieces.filter(piece => piece.prix <= 35);
-    console.log(pieces); 
-})
+
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+
+boutonFiltrer.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.prix <= 35;
+    });
+   console.log(piecesFiltrees)
+});
+
+//Correction Exercice
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+
+boutonDecroissant.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix;
+     });
+     console.log(piecesOrdonnees);
+});
+
+const boutonNoDescription = document.querySelector(".btn-nodesc");
+
+boutonNoDescription.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.description
+    });
+   console.log(piecesFiltrees)
+});
