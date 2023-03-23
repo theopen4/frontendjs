7957cc54db43ff15032389c59288739de7212cac
theopen4 <kielze86@gmail.com>
2023-03-23 13:50:07 +0100

    new code

---
+++ Frontend/pieces.js
@@ -0,0 +1,16 @@
+const reponse = await fetch('pieces-autos.json');
+const pieces = await reponse.json();
+const article = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
+const prixElement = document.createElement("p");
+prixElement.innerText = `prix: ${article.prix} $`;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie;
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiches.appendChild(categorieElement);
